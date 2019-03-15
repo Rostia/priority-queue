45ba5ad1916a9763d00a74bf9fdc1fc8496f2a2f
Ростислав <helloradros@gmail.com>
2019-03-15 16:33:56 +0300

    feat: PriorityQueue class created

--- src/queue.js
+++ src/queue.js
@@ -1,24 +1,27 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
-
+	constructor(maxSize = 30) {
+		this.maxSize = maxSize;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(this.maxSize === this.size()) throw new Error("MAX SIZE");
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if(this.size() === 0) throw new Error("queue is empty");
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
